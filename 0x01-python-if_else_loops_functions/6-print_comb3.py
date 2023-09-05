390acb634cda1a4bf2a94131ebd9cd6e75c2389a
Koffi Dodji Noumonvi <39010414+bravemaster3@users.noreply.github.com>
2023-09-05 13:09:51 +0000

    corrected 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for x in range(0, 9):
     for y in range(x+1, 10):
         if (x == 8 and y == 9):
-            print(f'{x:d}{y:d}')
+            print("{}{}".format(x,y))
         elif (x != y):
-            print(f'{x:d}{y:d}, ', end='')
+            print("{}{}, ".format(x,y), end='')
