7b02a136de04f391b92367ae68d927e38e749534
Koffi Dodji Noumonvi <39010414+bravemaster3@users.noreply.github.com>
2023-09-07 10:03:35 +0000

    add 3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -4,6 +4,6 @@ if __name__ == "__main__":
     sum = 0
     n_args = len(sys.argv)
     if (n_args - 1 > 0):
-        for i in range (1, n_args):
+        for i in range(1, n_args):
             sum += int(sys.argv[i])
     print(sum)
