ec4a630813f65481c0fa2826438ffcc673009ed1
Koffi Dodji Noumonvi <39010414+bravemaster3@users.noreply.github.com>
2023-09-07 10:02:54 +0000

    add 3

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    sum = 0
+    n_args = len(sys.argv)
+    if (n_args - 1 > 0):
+        for i in range (1, n_args):
+            sum += int(sys.argv[i])
+    print(sum)
