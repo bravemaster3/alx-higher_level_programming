9cfaeb35f4c7359852e1b0e22fead6148cdef06a
Koffi Dodji Noumonvi <39010414+bravemaster3@users.noreply.github.com>
2023-09-09 05:17:39 +0000

    correct 3 and 5

--- 0x03-python-data_structures/3-main.py
+++ 0x03-python-data_structures/3-main.py
@@ -4,3 +4,9 @@ print_reversed_list_integer = __import__(
 
 my_list = [1, 2, 3, 4, 5]
 print_reversed_list_integer(my_list)
+
+my_list = []
+print_reversed_list_integer(my_list)
+
+my_list = [1]
+print_reversed_list_integer(my_list)
