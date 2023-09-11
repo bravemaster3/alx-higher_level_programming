994cb897cd0e8a9ee2380d78b1bc25c9a7f872ba
Koffi Dodji Noumonvi <39010414+bravemaster3@users.noreply.github.com>
2023-09-11 11:06:02 +0000

    testing 100

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,5 +1,9 @@
 #include <Python.h>
 
+/**
+ * print_python_list_info - prints python list info
+ * @p: pointer to the object
+ */
 void print_python_list_info(PyObject *p)
 {
     Py_ssize_t size, i;
