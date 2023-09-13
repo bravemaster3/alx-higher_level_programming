d007b05fb55e8bacd1585333d7aaabfbe13366a2
Koffi Dodji Noumonvi <39010414+bravemaster3@users.noreply.github.com>
2023-09-13 12:26:02 +0000

    add 7

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
+
+"""
+Updates a dictionary when given a {key: value} pair
+"""
+
+
 def update_dictionary(a_dictionary, key, value):
     a_dictionary.update({key: value})
     return a_dictionary
-
