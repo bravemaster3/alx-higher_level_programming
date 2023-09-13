ebb5542dfb8a16502ef4a9bd9982a0dbda8f71d2
Koffi Dodji Noumonvi <39010414+bravemaster3@users.noreply.github.com>
2023-09-13 14:54:05 +0000

    Add 12

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    lookup = {
+        "I": 1,
+        "V": 5,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000
+    }
+    lst = list(roman_string)
+    repl = list(map(lambda x: lookup[x], lst))
+    if len(repl) >= 2:
+        for i in range(len(repl) - 1):
+            if (repl[i] < repl[i+1]):
+                repl[i+1] = repl[i+1] - repl[i]
+                repl[i] = 0
+
+    return sum(repl)
