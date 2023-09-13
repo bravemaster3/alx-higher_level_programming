dba87a2d3152fc2a89aa5789c7d1853edbe7609e
Koffi Dodji Noumonvi <39010414+bravemaster3@users.noreply.github.com>
2023-09-13 10:08:52 +0000

    corrected pycodestylde for 1

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -4,6 +4,7 @@
 Function that replaces a value by another in a list
 """
 
+
 def search_replace(my_list, search, replace):
-    new_list  = list(map(lambda x: replace if x == search else x, my_list))
-    return(new_list)
+    new_list = list(map(lambda x: replace if x == search else x, my_list))
+    return (new_list)
