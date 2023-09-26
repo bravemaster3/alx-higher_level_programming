bd73d5e0f0cb73507ea8d5dd356e23ba8dcf7bd2
Koffi Dodji Noumonvi <39010414+bravemaster3@users.noreply.github.com>
2023-09-26 13:37:47 +0000

    Add 3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""Class that defines a square
+
+This is a first class, and
+it has a method __init__ for setting its size
+
+"""
+
+
+class Square:
+    """This class creates a square
+
+    Initializes a given instance with a provided size
+
+    """
+
+    def __init__(self, size=0):
+        """This is an initializer
+        Args:
+            __size: private argument, the size of a square
+            size is checked first, and error raised if necessary
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """calculates the area of the square
+        Args:
+            No arguments
+        """
+        return self.__size ** 2
