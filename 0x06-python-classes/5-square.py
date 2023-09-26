8612147d09f92e8d934cc9bdeb783350a731fdfc
Koffi Dodji Noumonvi <39010414+bravemaster3@users.noreply.github.com>
2023-09-26 14:50:09 +0000

    Add 5

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+"""Class that defines a square
+
+This is a first class, and
+it has a method __init__ for setting its size
+
+"""
+
+
+class Square:
+    """This class creates a square
+
+    Initializes a given instance with a provided size
+
+    """
+
+    def __init__(self, size=0):
+        """This is an initializer
+        Args:
+            __size: private argument, the size of a square
+            size is checked first, and error raised if necessary
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """calculates the area of the square
+        Args:
+            No arguments
+        """
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+            return
+        for i in range(self.__size):
+            for j in range(self.__size):
+                print("#", end="")
+            print()
