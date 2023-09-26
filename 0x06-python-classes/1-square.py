a4a22e68ffd6a72de8bb2f3032e301dc913b0595
Koffi Dodji Noumonvi <39010414+bravemaster3@users.noreply.github.com>
2023-09-26 12:53:52 +0000

    Add 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Class that defines a square
+
+This is a first class, and
+it has a method __init__ for setting its size
+
+"""
+
+
+class Square:
+    """This class creates a square
+
+    It doesn't do anything
+
+    """
+
+    def __init__(self, size):
+        """This is an initializer
+
+        Args:
+            __size: private argument, the size of a square
+
+        """
+        self.__size = size
