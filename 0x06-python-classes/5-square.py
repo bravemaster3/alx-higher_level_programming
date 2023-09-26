336ca720bbb96805650d00690fceb6b1c624bf34
Koffi Dodji Noumonvi <39010414+bravemaster3@users.noreply.github.com>
2023-09-26 14:53:47 +0000

    Add 5

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -3,22 +3,22 @@
 
 This is a first class, and
 it has a method __init__ for setting its size
-
-Method __init__: This is an initializer
-    Args:
-        __size: private argument, the size of a square
-        size is checked first, and error raised if necessary
-Method area: calculates the area of the square
-    Args:
-        No arguments
-
-...
-
 """
 
 
 class Square:
+    """This class creates a square
+
+    Initializes a given instance with a provided size
+
+    """
+
     def __init__(self, size=0):
+        """This is an initializer
+        Args:
+            __size: private argument, the size of a square
+            size is checked first, and error raised if necessary
+        """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
@@ -27,6 +27,10 @@ class Square:
             self.__size = size
 
     def area(self):
+        """calculates the area of the square
+        Args:
+            No arguments
+        """
         return self.__size ** 2
 
     @property
