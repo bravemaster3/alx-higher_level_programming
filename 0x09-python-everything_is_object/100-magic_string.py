6ac09a349a9e5666089cf550dacde1be3ef47a2d
Koffi Dodji Noumonvi <39010414+bravemaster3@users.noreply.github.com>
2023-10-03 14:42:11 +0000

    add 100

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 i = -1
 def magic_string():
-    global i
-    i += 1
-    return "BestSchool, " * i + "BestSchool"
+    global i; i += 1; return "BestSchool, " * i + "BestSchool"
