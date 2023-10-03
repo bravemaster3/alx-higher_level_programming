aa477886fdf398cfd656bb6779d303d5652acea0
Koffi Dodji Noumonvi <39010414+bravemaster3@users.noreply.github.com>
2023-10-03 17:07:01 +0000

    Add 101

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Locked class except for first_name attribute
+"""
+
+
+class LockedClass:
+    """class definition here"""
+
+    def __setattr__(self, name, value):
+        """setting attribute"""
+        if name != "first_name":
+            raise AttributeError("{} object has no attribute '{}'"
+                                 .format(self.__class__.__name__, name))
+        super().__setattr__(name, value)
