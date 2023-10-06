21bb1bb791d351fc9f93a9b4e49d1387f9405852
Koffi Dodji Noumonvi <39010414+bravemaster3@users.noreply.github.com>
2023-10-06 20:28:36 +0000

    Add 101, incorrect tests

---
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""
+Matrix multiplication using numpy
+"""
+
+import numpy as np
+
+
+def lazy_matrix_mul(m_a, m_b):
+    """Lazy lazy boy"""
+    arr1 = np.array(m_a)
+    arr2 = np.array(m_b)
+    result = arr1 @ arr2
+    return result.tolist()
