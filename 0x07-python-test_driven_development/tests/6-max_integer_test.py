99da4dbceea1f0317e8a29e35fd5032099cbee6d
Koffi Dodji Noumonvi <39010414+bravemaster3@users.noreply.github.com>
2023-10-06 19:54:57 +0000

    corrected task 5

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -20,3 +20,12 @@ class TestMaxInteger(unittest.TestCase):
         """check if the correct value is printed"""
         self.assertEqual(max_integer([12, 289, 0, 45]), 289)
         self.assertEqual(max_integer([-12, -289, -0, -45]), 0)
+
+    def test_max_end(self):
+        self.assertEqual(max_integer([12, 34, 56, 89]), 89)
+
+    def test_max_beginning(self):
+        self.assertEqual(max_integer([89, 56, 34, 12]), 89)
+
+    def test_list_one_element(self):
+        self.assertEqual(max_integer([89]), 89)
