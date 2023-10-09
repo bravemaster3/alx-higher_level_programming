60c42f729b300d9bc8ac202df72db5fcd71f230d
Koffi Dodji Noumonvi <39010414+bravemaster3@users.noreply.github.com>
2023-10-09 16:34:19 +0000

    Add 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+definition of class BaseGeometry
+"""
+
+
+class BaseGeometry():
+    """class definition starts here"""
+    pass
+
+    def area(self):
+        raise Exception("area() is not implemented")
