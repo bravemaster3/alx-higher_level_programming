c2eb59c21c197f5f0c87348f5fd072c47a7f2796
Koffi Dodji Noumonvi <39010414+bravemaster3@users.noreply.github.com>
2023-10-09 15:56:39 +0000

    Add 3

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+Module containing a function for checking
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """function definition here"""
+    return isinstance(obj, a_class)
