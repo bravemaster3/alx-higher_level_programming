cd6af195276d7585bf27e4608df3d3f9479eb1cc
Koffi Dodji Noumonvi <39010414+bravemaster3@users.noreply.github.com>
2023-10-09 21:58:59 +0000

    use setattr instead

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -7,6 +7,6 @@ A function that adds a new attribute
 def add_attribute(obj, name, value):
     """function defintion here"""
     if hasattr(obj, "__dict__"):
-        obj.name = value
+        setattr(obj, name, value)
     else:
         raise TypeError("can't add new attribute")
