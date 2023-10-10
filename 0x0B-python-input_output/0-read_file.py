cf15b1d1ba08be0c3ddbcff021baa7ab18be2f92
Koffi Dodji Noumonvi <39010414+bravemaster3@users.noreply.github.com>
2023-10-10 15:33:47 +0000

    Add 0

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Module contains a function for reading a file
+Args:
+    filename: string
+"""
+
+
+def read_file(filename=""):
+    """function definition"""
+    with open(filename, "r", encoding="utf-8") as f:
+        print(f.read())
---
+++ 0x0B-python-input_output/README.md
@@ -0,0 +1,2 @@
+# I/O in python
+Bring some in, take some out
