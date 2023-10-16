3f7f29917713a5245bb8b6b0aaea31f4775ea1d5
Koffi Dodji Noumonvi <39010414+bravemaster3@users.noreply.github.com>
2023-10-16 15:16:35 +0000

    correct to_json_string

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -24,7 +24,7 @@ class Base():
     @staticmethod
     def to_json_string(list_dictionaries):
         """converts a list of dict to json string"""
-        if list_dictionaries is None:
+        if list_dictionaries is None or list_dictionaries == [{}]:
             return "[]"
         return json.dumps(list_dictionaries)
 
