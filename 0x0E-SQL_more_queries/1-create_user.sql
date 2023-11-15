3f87271ac26472255e3de7b5a82db0fbe09ba18b
Koffi Dodji Noumonvi <39010414+bravemaster3@users.noreply.github.com>
2023-11-15 15:17:57 +0000

    Add 1 Create user with priviledges

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,4 @@
+-- Script that creates mysql user with all priviledges
+CREATE USER IF NOT EXISTS user_0d_1@localhost
+IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEDGES ON *.* TO 'user_0d_1'@'localhost';
