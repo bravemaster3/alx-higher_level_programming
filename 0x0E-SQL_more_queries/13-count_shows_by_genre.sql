04d73f4706cccf56d31f599fb0321cc7ff152e7d
Koffi Dodji Noumonvi <39010414+bravemaster3@users.noreply.github.com>
2023-11-16 03:01:12 +0000

    Add 13 unchecked

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,5 +1,5 @@
 -- Number of shows by genre
-SELECT tv_genres.name AS genre, COUNT(tv_show_genre.show_id) as number_of_shows
+SELECT tv_genres.name AS genre, COUNT(tv_show_genres.show_id) as number_of_shows
 FROM tv_genres
 LEFT JOIN tv_show_genres
 ON tv_show_genres.genre_id = tv_genres.id
