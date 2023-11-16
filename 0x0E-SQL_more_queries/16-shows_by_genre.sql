cfd7805bbcb71966d1b41e1f95f57d15be2206a9
Koffi Dodji Noumonvi <39010414+bravemaster3@users.noreply.github.com>
2023-11-16 04:17:37 +0000

    Add 17 Subquery genre not show dexter

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -2,7 +2,6 @@
 -- from the database hbtn_0d_tvshows
 SELECT tv_shows.title, tv_genres.name
 FROM tv_genres
-JOIN tv_show_genres sg
-JOIN tv_shows
-WHERE sg.genre_id = tv_genres.id AND sg.show_id = tv_shows.id
-ORDER BY title ASC, name ASC;
+LEFT JOIN tv_show_genres sg ON sg.genre_id = tv_genres.id
+RIGHT JOIN tv_shows ON sg.show_id = tv_shows.id
+ORDER BY tv_shows.title ASC, tv_genres.name ASC;
