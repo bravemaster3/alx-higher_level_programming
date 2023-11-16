0ed7afbef64ed9357ed7d9d8233a0274eff2e3e2
Koffi Dodji Noumonvi <39010414+bravemaster3@users.noreply.github.com>
2023-11-16 09:11:17 +0000

    JOIN instead of RIGHT JOIN for task 13

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,7 +1,7 @@
 -- Number of shows by genre LEFT JOIN GROUP BY and count
 SELECT tv_genres.name AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows
 FROM tv_genres
-LEFT JOIN tv_show_genres
+JOIN tv_show_genres
 ON tv_show_genres.genre_id = tv_genres.id
 GROUP BY tv_genres.name
 ORDER BY number_of_shows DESC;
