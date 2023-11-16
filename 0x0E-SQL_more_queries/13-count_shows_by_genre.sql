63d0c78e971ae0c79a671dc26a1cc1f4194afb7b
Koffi Dodji Noumonvi <39010414+bravemaster3@users.noreply.github.com>
2023-11-16 03:23:03 +0000

    Add 14 JOIN 3 tables

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -3,5 +3,5 @@ SELECT tv_genres.name AS genre, COUNT(tv_show_genres.show_id) as number_of_shows
 FROM tv_genres
 LEFT JOIN tv_show_genres
 ON tv_show_genres.genre_id = tv_genres.id
-GROUP BY tv_genres.id
+GROUP BY tv_genres.name
 ORDER BY number_of_shows DESC;
