b76c6f7452a876e174f3820ceac8deb48185f6ee
Koffi Dodji Noumonvi <39010414+bravemaster3@users.noreply.github.com>
2023-11-16 09:06:43 +0000

    replaced as by AS

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -1,6 +1,6 @@
 -- script that lists all genres in the database
 -- hbtn_0d_tvshows_rate by their rating
-SELECT g.name, SUM(sr.rate) as rating
+SELECT g.name, SUM(sr.rate) AS rating
 FROM tv_genres g
 JOIN tv_show_genres sg ON sg.genre_id = g.id
 JOIN tv_show_ratings sr ON sg.show_id = sr.show_id
