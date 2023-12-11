eadd3187191f52570f99ac9c08387671e05f2054
Koffi Dodji Noumonvi <39010414+bravemaster3@users.noreply.github.com>
2023-12-11 14:55:34 +0000

    Add 5 parseint when possible

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+/* Write a script that prints My number: <first argument converted in integer>
+if the first argument can be converted to an integer: */
+const allArgs = process.argv.slice(2);
+const isInt = function (myVal) {
+  return (/^\d+\.*\d+$/.test(myVal));
+};
+let msg;
+if (isInt(allArgs[0])) {
+  msg = `My number: ${parseInt(allArgs[0])}`;
+} else {
+  msg = 'Not a number';
+}
+console.log(msg);
