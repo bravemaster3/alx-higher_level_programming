d1daebf60b5d06d15e7eab2de5ce82844109ec3d
Koffi Dodji Noumonvi <39010414+bravemaster3@users.noreply.github.com>
2023-12-11 15:46:58 +0000

    Add 9 Addition function

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+/* Write a script that prints the addition of 2 integers */
+const add = function(a, b) {
+  return a + b;
+}
+const allArgs = process.argv.slice(2);
+console.log(add(parseInt(allArgs[0]), parseInt(allArgs[1])));
