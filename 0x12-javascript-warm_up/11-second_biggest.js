f121c568b5c3018e828e55765e19567497cbd0ee
Koffi Dodji Noumonvi <39010414+bravemaster3@users.noreply.github.com>
2023-12-11 17:02:29 +0000

    Add 11

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+/* Write a script that searches the second biggest integer in the list of arguments */
+const allArgs = process.argv.slice(2);
+let sec = 0;
+let first;
+let swap;
+if (allArgs.length > 1) {
+  first = allArgs[0]; /* Number.MIN_SAFE_INTEGER */
+  sec = allArgs[1];
+  if (sec > first) {
+    swap = first;
+    first = sec;
+    sec = swap;
+  }
+  for (i in allArgs) {
+    if (parseInt(allArgs[i]) > first) {
+      sec = first;
+      first = parseInt(allArgs[i]);
+    }
+    if (parseInt(allArgs[i]) != first && parseInt(allArgs[i]) > sec) {
+      sec = parseInt(allArgs[i]);
+    }
+  }
+}
+console.log(sec);
