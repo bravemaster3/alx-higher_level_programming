82a38ff748a185c5f1fa3952955657ec823ab115
Koffi Dodji Noumonvi <39010414+bravemaster3@users.noreply.github.com>
2023-12-11 15:28:46 +0000

    Add 8 print a square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+/* Write a script that prints a square */
+const x = process.argv[2];
+if (x === undefined || isNaN(parseInt(x))) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < parseInt(x); i++) {
+    console.log('x'.repeat(x));
+  }
+}
