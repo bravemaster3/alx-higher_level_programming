1e2cc3f84e07336cc5bd453819880155b4b1ee92
Koffi Dodji Noumonvi <39010414+bravemaster3@users.noreply.github.com>
2023-12-11 14:12:42 +0000

    Add 3 print only first argument

---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+/* Write a script that prints the first argument passed to it */
+const allArgs = process.argv.slice(2);
+let msg;
+if (allArgs.length === 0) {
+  msg = 'No argument';
+} else {
+  msg = allArgs[0];
+}
+console.log(msg);
