98f66f8246a1725fca8f0132e5769a7b79588dcd
Koffi Dodji Noumonvi <39010414+bravemaster3@users.noreply.github.com>
2023-12-12 19:38:26 +0000

    Add 102

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -2,7 +2,6 @@
 /* script that concatenates 2 files */
 const fs = require('fs');
 const args = process.argv.slice(2);
-console.log(args[0]);
 const fileA = fs.readFileSync(args[0]);
 const fileB = fs.readFileSync(args[1]);
 fs.writeFileSync(args[2], fileA + fileB);
