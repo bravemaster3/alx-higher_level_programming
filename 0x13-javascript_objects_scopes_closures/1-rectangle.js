5217bd9c14d52a59e6a83cc429a9899ab998d20a
Koffi Dodji Noumonvi <39010414+bravemaster3@users.noreply.github.com>
2023-12-12 11:21:43 +0000

    Add 1 width and height

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+/* class Rectangle that defines a rectangle */
+class Rectangle {
+  width;
+  height;
+
+  constructor(w, h) {
+    this.width = w;
+    this.height = h;
+  }
+}
+module.exports = Rectangle;
