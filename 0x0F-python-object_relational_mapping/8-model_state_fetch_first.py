2e11942fe4adb49e4810b8c0d4b28b8ec8e6a3a4
Koffi Dodji Noumonvi <39010414+bravemaster3@users.noreply.github.com>
2023-12-15 22:48:11 +0000

    Add 8

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Script that prints the first State object from the database hbtn_0e_6_usa
+"""
+
+if __name__ == "__main__":
+    from model_state import Base, State
+    import sqlalchemy
+    from sqlalchemy import (create_engine)
+    from sqlalchemy.orm import sessionmaker
+    import sys
+
+    USER = sys.argv[1]
+    PASS = sys.argv[2]
+    DB_NAME = sys.argv[3]
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        USER, PASS, DB_NAME), pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    first_row = session.query(State).first()
+
+    print("{}: {}".format(first_row.id, first_row.name))
+
+    session.close
