a4f6fc37a999852d2c1e005c7a410a759874e914
Koffi Dodji Noumonvi <39010414+bravemaster3@users.noreply.github.com>
2023-12-15 14:39:32 +0000

    Add 1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-Script that lists all states from the database hbtn_0e_0_usa
+Script that lists all states with a name
+starting with N (upper N) from the database hbtn_0e_0_usa
 """
 
 if __name__ == "__main__":
@@ -12,7 +13,10 @@ if __name__ == "__main__":
     DB_NAME = sys.argv[3]
     db = MySQLdb.connect(user=USER, passwd=PASS, db=DB_NAME)
     cur = db.cursor()
-    cur.execute("SELECT id, name FROM states ORDER BY id")
+    cur.execute("""SELECT id, name
+                FROM states
+                WHERE name LIKE %s
+                ORDER BY id""", ('N%',))
     all = cur.fetchall()
     for i in all:
         print(i)
