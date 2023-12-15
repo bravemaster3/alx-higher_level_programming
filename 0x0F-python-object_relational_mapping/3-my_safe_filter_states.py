87c86a682c3d9c8105b58f062b81da7e923a0562
Koffi Dodji Noumonvi <39010414+bravemaster3@users.noreply.github.com>
2023-12-15 15:13:38 +0000

    Add 3

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Script that lists all states with a name
+starting with N (upper N) from the database hbtn_0e_0_usa
+"""
+
+if __name__ == "__main__":
+    import MySQLdb
+    import sys
+
+    USER = sys.argv[1]
+    PASS = sys.argv[2]
+    DB_NAME = sys.argv[3]
+    STATE = sys.argv[4]
+    db = MySQLdb.connect(user=USER, passwd=PASS, db=DB_NAME)
+    cur = db.cursor()
+    cur.execute("""SELECT id, name
+                FROM states
+                WHERE BINARY name = %s
+                ORDER BY id""", (STATE,))
+    all = cur.fetchall()
+    for i in all:
+        print(i)
