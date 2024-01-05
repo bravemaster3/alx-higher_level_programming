c88b6ce3816290ecce0f4c0e2a8b9eaa5282bcda
Koffi Dodji Noumonvi <39010414+bravemaster3@users.noreply.github.com>
2024-01-05 15:56:54 +0000

    Add 1

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that takes in a URL, sends a request to that URL, and displays the body content of 200 OK
-curl -s "$1"
+[ "$(curl -s -o /dev/null -w "%{http_code}" "$1")" -eq 200 ]&& curl -s "$1"
