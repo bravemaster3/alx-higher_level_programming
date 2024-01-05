8ae90b02103fb737146cf41efb5cfb257c925878
Koffi Dodji Noumonvi <39010414+bravemaster3@users.noreply.github.com>
2024-01-05 15:39:52 +0000

    Add 0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
-# Bash script that takes in a URL, sends a request to that URL,
-# and displays the size of the body of the response
-
-#Extract the headers
-headers=$(curl -I -s "$1")
-
-# Extract the content length
-echo "$headers" | grep -i "Content-Length" | awk '{print $2}'
+# Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
+curl -I -s "$1" | grep -i "Content-Length" | awk '{print $2}'
