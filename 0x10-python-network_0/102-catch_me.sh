a697a40f89f11174ecada090838562425501c9ec
Koffi Dodji Noumonvi <39010414+bravemaster3@users.noreply.github.com>
2024-01-06 02:45:51 +0000

    Add 102 Caught you! following redirections and findin the Origin Header needed

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Bash script that sends a JSON POST request to a URL passed as the first argument, and displays the body of the response.
+# Bash script that makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with a message containing You got me!, in the body of the response.
 curl -sLX PUT 0.0.0.0:5000/catch_me_2 -d "user_id=98" -H "Origin: School"
