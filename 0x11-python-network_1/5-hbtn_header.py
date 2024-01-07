23df1e02747db43610d2c4aa89d65c192fda5dd2
Koffi Dodji Noumonvi <39010414+bravemaster3@users.noreply.github.com>
2024-01-07 20:20:27 +0000

    Add 5 used getattr

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -11,5 +11,5 @@ if __name__ == "__main__":
     import sys
     my_url = sys.argv[1]
     req = requests.get(my_url)
-    header = req.headers['X-Request-Id']
+    header = getattr(req.headers, 'X-Request-Id')
     print(header)
