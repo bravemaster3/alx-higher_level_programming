92a5dadae6ea20c2e854deaa2d852f6d1cb60ead
Koffi Dodji Noumonvi <39010414+bravemaster3@users.noreply.github.com>
2024-01-07 20:07:32 +0000

    Add 5

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Python script that takes in a URL,
+sends a request to the URL and displays
+the value of the variable X-Request-Id in the response header
+"""
+
+
+if __name__ == "__main__":
+    import requests
+    import sys
+    my_url = sys.argv[1]
+    req = requests.get(my_url)
+    print("Body response:")
+    header = req.headers['X-Request-Id']
+    print(header)
