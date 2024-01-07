e5d64b5463d7cbe44bcbac65020db48d4a3e349a
Koffi Dodji Noumonvi <39010414+bravemaster3@users.noreply.github.com>
2024-01-07 21:41:32 +0000

    Corrected order of sys.argv args

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -8,17 +8,14 @@ of the repository “rails” by the user “rails”
 if __name__ == "__main__":
     import requests
     import sys
-    user = sys.argv[1]
-    repos = sys.argv[2]
+    user = sys.argv[2]
+    repos = sys.argv[1]
     url = f"https://api.github.com/repos/{user}/{repos}/commits"
     response = requests.get(url, params={'per_page': 10})
     json_resp = response.json()
-    try:
-        for commit in json_resp:
-            sha = commit.get("sha")
-            author_name = commit.get("commit").get("author").get("name")
-            # date = commit["commit"]["author"]["date"]
-            # print(f"{sha}: {author_name} {date}")
-            print(f"{sha}: {author_name}")
-    except Exception:
-        pass
+    for commit in json_resp:
+        sha = commit["sha"]
+        author_name = commit["commit"]["author"]["name"]
+        # date = commit["commit"]["author"]["date"]
+        # print(f"{sha}: {author_name} {date}")
+        print(f"{sha}: {author_name}")
