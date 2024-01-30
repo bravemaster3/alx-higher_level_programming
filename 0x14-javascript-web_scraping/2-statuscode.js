6c4d89dc210463fac139077d5ec0b28d8de59ac9
Koffi Dodji Noumonvi <39010414+bravemaster3@users.noreply.github.com>
2024-01-30 20:14:35 +0000

    Add 2

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -2,7 +2,8 @@
 const process = require('process');
 const request = require('request');
 const url = process.argv[2];
-request = new Request(url);
 request(url, (error, response, body) => {
-    console.log("code: ${response.statusCode}");
-})
+  if (!error) {
+    console.log(`code: ${response.statusCode}`);
+  }
+});
