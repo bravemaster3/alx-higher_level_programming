661cae60c49f57b2dcc28768e83aeb8843c9177b
Koffi Dodji Noumonvi <39010414+bravemaster3@users.noreply.github.com>
2024-01-31 18:39:26 +0000

    Add 101

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -3,17 +3,20 @@ const process = require('process');
 const request = require('request');
 const url = 'https://swapi-api.alx-tools.com/api/films';
 const number = process.argv[2];
-request(url, (error, response, body) => {
+request(url, async (error, response, body) => {
   if (!error) {
     const films = JSON.parse(body).results;
     //   console.log(people);
     const keys = Object.keys(films);
     const myFilm = films[keys[number - 1]];
     for (const char of myFilm.characters) {
-      request(char, (error, response, body) => {
-        if (!error) {
-          console.log(JSON.parse(body).name);
-        }
+      await new Promise((resolve) => {
+        request(char, (error, response, body) => {
+          if (!error) {
+            console.log(JSON.parse(body).name);
+          }
+          resolve();
+        });
       });
     }
   }
