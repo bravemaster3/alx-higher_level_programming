9651b91fc2bd06809bca3314d40eeddcdacb7ea2
Koffi Dodji Noumonvi <39010414+bravemaster3@users.noreply.github.com>
2024-02-14 02:14:39 +0000

    Fixed some stuff and deleted pycache

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,11 +8,9 @@ subreddit. If an invalid subreddit is given, the function should return 0.
 
 def number_of_subscribers(subreddit):
     """Function definition"""
+    import requests
     base_url = "https://www.reddit.com"
     end_point = "/subreddits/search"
-    # query = ".json?q=programming&limit=1"
-    import json
-    import requests
 
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) \
                AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 \
@@ -20,8 +18,11 @@ def number_of_subscribers(subreddit):
     req = requests.get(base_url+end_point + f".json?q={subreddit}&limit=1",
                        headers=headers, allow_redirects=False)
     result = req.json()
+    if req.status_code != 200:
+        return 0
     n_subs = 0
-    if result['data']['children'][0]['data']['display_name'] == subreddit:
+    if len(result['data']['children']) > 1 and \
+        result['data']['children'][0]['data']['display_name'] == subreddit:
         n_subs = result['data']['children'][0]['data']['subscribers']
 
     return n_subs
